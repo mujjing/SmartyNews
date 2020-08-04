e875b1c6194775676854f41c1504c60865bb37de
田 知勲 <j-jihoon@winas.jp>
2020-08-04 18:31:11 +0900

    smartMenuTap작성

---
+++ JhSmartNews/JhSmartNews/ViewController/News1ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  News1ViewController.swift
+//  JhSmartNews
+//
+//  Created by Jh's Macbook Pro on 2020/08/04.
+//  Copyright © 2020 전지훈. All rights reserved.
+//
+
+import UIKit
+
+class News1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
---
+++ JhSmartNews/JhSmartNews/ViewController/News4ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  News4ViewController.swift
+//  JhSmartNews
+//
+//  Created by Jh's Macbook Pro on 2020/08/04.
+//  Copyright © 2020 전지훈. All rights reserved.
+//
+
+import UIKit
+
+class News4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     view.backgroundColor = .green
+    }
+
+}
